da1eb6bc49d26d94a3861fc35bcececaaf4a2cef
Endre Karlson <endre.karlson@hp.com>
2015-03-10 12:04:01 +0100

    Switch to asyncproc from dom96 to not block

--- src/private/run.nim
+++ src/private/run.nim
@@ -1,26 +1,29 @@
-import json, os, strutils
+import asyncdispatch, json, os, strutils
 import uuid
 
-import playpen
+import asyncproc
 
 type
   RunResult* = enum
     Success = 0, Error = 1
 
-  Run* = object
+  Run = ref RunObj
+  RunObj* = object
     id*: string
     input*: string
     output*: string
     result*: RunResult
+    code: int
     version*: string
     compilerOptions: JsonNode
-    snippetDir: string
 
 var defaultOpts = %{
   "threads": %false,
   "stackTrace": %false,
   "checks": %true
 }
+
+var executor = newAsyncExecutor()
 
 proc `%`*(runResult: RunResult): JsonNode =
   %($runResult)
@@ -77,6 +80,20 @@ proc toArgs*(compilerOptions: JsonNode): seq[string] =
 
     result.add("--$#:$#" % [k, asString])
 
+proc playpenCmd(binPath: string, chrootPath: string): seq[string] =
+  result = @[
+    findExe"sudo",
+    binPath,
+    chrootPath,
+    "--mount-proc",
+    "--user=nim",
+    "--timeout=5",
+    "--syscalls-file=whitelist",
+    "--devices=/dev/urandom:r,/dev/null:w",
+    "--memory-limit=128",
+    "--"
+  ]
+
 proc execRun*(run: var Run, playPath: string, versionsPath: string) =
   let
     chrootPath = joinPath(versionsPath, run.version)
@@ -114,10 +131,10 @@ proc execRun*(run: var Run, playPath: string, versionsPath: string) =
     #"--cc:ucc",
   ]
 
-  let opts = run.compilerOptions.toArgs
-  echo "Compiler options for " & run.id & ": " & $opts
+  #let opts = run.compilerOptions.toArgs
+  #echo "Compiler options for " & run.id & ": " & $opts
 
-  args.add(opts)
+  #args.add(opts)
 
   args.add(@[
     "compile",
@@ -125,13 +142,78 @@ proc execRun*(run: var Run, playPath: string, versionsPath: string) =
     runFile
   ])
 
-  let
-    (output, code) = runInPen(playPath, chrootPath, cmd = args)
+  #var foo = runInPen(playPath, chrootPath, cmd = args)
 
-  if output.len > 1000:
-    run.result = Error
-    run.output = "Output too long..."
-  else:
-    run.output = output
-    if code.int != 0:
-      run.result = Error
+  #if output.len > 1000:
+  #  run.result = Error
+  ##  run.output = "Output too long..."
+  #else:
+  #  run.output = output
+  #  if code.int != 0:
+  #    run.result = Error
+
+proc execute*(self: Run, playPath: string, versionsPath: string) {.async.} =
+  self.output = ""
+
+  proc cb(event: ProcessEvent) {.async.} =
+    # Do necassary actions based on event kind.
+    case event.kind:
+      of ProcessStdout, ProcessStderr:
+        self.output.add(event.data)
+      of ProcessEnd:
+        self.code = event.code
+        self.result = if event.code == 0: Success else: Error
+
+      else: discard
+
+  let
+    chrootPath = joinPath(versionsPath, self.version)
+    runDir = joinPath("/", "mnt", "runs", self.id.replace("-", "_"))
+    runFile = joinPath(runDir, "file.nim")
+    nimPath = joinPath("/usr/local/nim/versions", self.version, "bin", "nim")
+
+  echo "chroot " & chrootPath
+  let path = joinPath(chrootPath, runDir)
+  echo ("Creating directory: " & path)
+  createDir(path)
+
+  echo "Writing snippet"
+  writeFile(joinPath(chrootPath, runFile), self.input)
+  echo("Snippet written to: " & runFile)
+
+  var cmd = playpencmd(playPath, chrootPath)
+  cmd.add(@[
+    nimPath,
+    "--nimcache:/tmp", # Same as below
+    "-o:/tmp/program", # Store in /tmp since it's in-memory mount by playpen
+    "--lineTrace:off",
+    "--fieldChecks:off",
+    "--rangeChecks:on",
+    "--boundChecks:on",
+    "--overflowChecks:on",
+    "--assertions:on",
+    "--floatChecks:off",
+    "--nanChecks:on",
+    "--infChecks:off",
+    "--opt:none",
+    "--warnings:off",
+    "--hints:off",
+    "--threadanalysis:off",
+    "--verbosity:0",
+    #"--cc:ucc",
+  ])
+
+  let opts = self.compilerOptions.toArgs
+  echo "Compiler options for " & self.id & ": " & $opts
+
+  cmd.add(opts)
+
+  cmd.add(@[
+    "compile",
+    "--run",
+    runFile
+  ])
+
+  echo ("Calling: " & join(cmd, " "))
+  executor.start()
+  await executor.exec(findExe("sudo"), progress = cb, args = cmd)
